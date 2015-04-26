88ca643be441f880ea2ed53b136fb77e93643bbe
rbcap <errajubanerjee@gmail.com>
2015-04-26 12:47:29 +0530

    Update response_parser.sh
    added input from user

--- response_parser.sh
+++ response_parser.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 #log_file defines the log location
-log_file="log_parser.log"
+echo -n "Enter the log file location -->"
+read log_file
+echo "log file name/path = " $log_file
 
+#code
 echo "number of HTTP 404 errors in sample log file"
 grep "^.*:80" $log_file | awk '($10 ~ /404/)' | awk '{print $10}' | sort | uniq -c
 
